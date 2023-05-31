66d0c3e8b97b61a81e33352fcdeab3e694bdc8ad
Casey <k11tec@tutanota.com>
2023-05-31 11:14:51 +0200

    mise en place de l'historique

--- BloomApp/app/src/main/java/com/example/bloomapp/MainActivity.java
+++ BloomApp/app/src/main/java/com/example/bloomapp/MainActivity.java
@@ -1,41 +1,52 @@
-package com.example.bloomapp;
+package com.example.bloomapp; // Remplacez com.example.bloomapp par le nom de votre propre package
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    TextView txt;
-    Button btn;
-
-
-
+    private Button btn;
+    private LinearLayout historiqueLinearLayout;
+    private ArrayList<Integer> historique;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        txt = findViewById(R.id.TextView);
         btn = findViewById(R.id.Button);
+        historiqueLinearLayout = findViewById(R.id.historiqueLinearLayout);
+
+        historique = new ArrayList<>();
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Random random = new Random();
-                int valeur = random.nextInt(100);
-                txt.setText(Integer.toString(valeur));
+                int valeur = new Random().nextInt(100);
+                historique.add(valeur);
+                if (historique.size() > 10) {
+                    historique.remove(0);
+                }
+                afficherHistorique();
             }
         });
+    }
+
+    private void afficherHistorique() {
+        historiqueLinearLayout.removeAllViews();
+
+        for (int i = 0; i < historique.size(); i++) {
+            TextView textView = new TextView(this);
+            textView.setText(String.valueOf(historique.get(i)));
+            historiqueLinearLayout.addView(textView);
+        }
     }
 }
